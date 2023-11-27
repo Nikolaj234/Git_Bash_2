81ccf63a44526d4df227a2bf46aac9519194bb29
ip320-15 <niikolajkirillov777@gmail.com>
2023-11-27 15:09:47 +0300

    Добавлен  вывод сообщения Привет Мир

--- Git_Bash_2/Program.cs
+++ Git_Bash_2/Program.cs
@@ -6,7 +6,7 @@ namespace Git_Bash_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Привет мир");
         }
     }
 }
